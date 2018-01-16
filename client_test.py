5b74c0c3d090be0a6e1c9e79dad37069edfae56d
cookie <axel.neergaard@gmail.com>
2018-01-16 13:26:12 +0200

    Adds script with example socket use

---
+++ client_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+
+import socket, os
+
+print("PID: {}".format(os.getpid()))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("localhost", 9988))
+s.sendall(b'Testing')
+s.close()
+
