cfa9f85afbf5919dcb80b21fb5444da83040d3a4
Axel Neergaard <axel.neergaard@aalto.fi>
2018-01-19 01:10:13 +0200

    Script - 2018/01/19-01:10:13

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -10,9 +10,10 @@ git push
 pi_ip=$(cat pi_ip | grep 192)
 id_rsa=$(cat pi_ip | grep .ssh)
 tbot=$(cat pi_ip | grep tbot)
+query="cd ${tbot} && git pull"
 
 # Pull changes on RPi
-ssh -i ${id_rsa} pi@${pi_ip} 'cd ${tbot} && git pull'
+ssh -i ${id_rsa} pi@${pi_ip} ${query}
 
 if [[ $? -eq 0 ]]; then
     echo "Success"
