82f2c6c6cd9cbb2a7b3b445a83e820e02d5aefc3
Cookie <axel.neergaard@aalto.fi>
2018-01-18 14:58:29 +0000

    Changed motion detection message

--- client_test.py
+++ client_test.py
@@ -6,6 +6,6 @@ print("PID: {}".format(os.getpid()))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 9988))
-s.sendall(b'Testing')
+s.sendall(b'Something moved!')
 s.close()
 
